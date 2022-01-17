60c48ef2db9a0559aa24d04011b44f5442846e54
abulbashar98 <abulbashar98116@gmail.com>
2022-01-17 13:18:40 +0600

    random-user

--- js/jsonplaceholder.js
+++ js/jsonplaceholder.js
@@ -1,21 +1,21 @@
 const loadComments = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/comments');
-    const data = await response.json();
-    displayComments(data);
+  const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+  const data = await response.json();
+  displayComments(data);
 }
 
 
 
 
 const displayComments = comments => {
-    console.log(comments)
+  console.log(comments)
 
-    const commentsContainerDiv = document.getElementById('comments-container')
+  const commentsContainerDiv = document.getElementById('comments-container')
 
-    comments.forEach(comment => {
+  comments.forEach(comment => {
 
-        const div = document.createElement('div')
-        div.innerHTML = `
+    const div = document.createElement('div')
+    div.innerHTML = `
         <div   class="card"  onclick="loadCommentById('${comment.id}')" >
         <img src="..." class="card-img-top" alt="...">
         <div  class="card-body">
@@ -26,30 +26,31 @@ const displayComments = comments => {
         </div>
       </div>
 `
-        commentsContainerDiv.appendChild(div)
-    });
+    commentsContainerDiv.appendChild(div)
+  });
 }
 
 
 
 
 const loadCommentById = async id => {
-    const url = `https://jsonplaceholder.typicode.com/comments/${id}`
-    const response = await fetch(url);
-    const singleCommentDetail = await response.json();
-    displayCommentDetail(singleCommentDetail);
+  const url = `https://jsonplaceholder.typicode.com/comments/${id}`
+  const response = await fetch(url);
+  const singleCommentDetail = await response.json();
+  displayCommentDetail(singleCommentDetail);
 }
 
 
 const displayCommentDetail = comment => {
 
-    const commentDetailContainer = document.getElementById('comment-detail')
-    commentDetailContainer.textContent = '';
+  const commentDetailContainer = document.getElementById('comment-detail')
+  commentDetailContainer.textContent = '';
 
 
-    const div = document.createElement('div')
-    div.classList.add('card')
-    div.innerHTML = `
+
+  const div = document.createElement('div')
+  div.classList.add('card')
+  div.innerHTML = `
     <div  class="card-body bg-info">
     <h5 class="card-title">Name: ${comment.name}</h5>
     <h6>Email: ${comment.email}</h6>
@@ -57,9 +58,7 @@ const displayCommentDetail = comment => {
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
     `
-    commentDetailContainer.appendChild(div)
-
-
+  commentDetailContainer.appendChild(div)
 
 }
 
---
+++ js/randomuser.js
@@ -0,0 +1,66 @@
+
+
+const loadRandomUser = async () => {
+    const response = await fetch('https://randomuser.me/api/?results=50');
+    const data = await response.json();
+    displayRandomUsers(data.results)
+}
+
+
+
+const displayRandomUsers = users => {
+
+    const userInformationContainerDiv = document.getElementById('users-container');
+
+    users.forEach(user => {
+        console.log(user)
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img src="${user.picture.large}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Name:- ${user.name.title} ${user.name.first} ${user.name.last}</h5>
+                <h6 class="card-title">Email:- ${user.email}</h6>
+                <p class="card-text">Country:- ${user.location.country}</p>
+                <a href="#" class="btn btn-primary" onclick="loadRandomuserById('${user.id.value}')">Details</a>
+            </div>
+        </div>
+        `
+        userInformationContainerDiv.appendChild(div);
+
+    });
+}
+
+const loadRandomuserById = async userId => {
+
+    const url = `https://randomuser.me/api/?results=50/${userId}`
+    const response = await fetch(url)
+    const user = await response.json();
+    displayUserDetails(user.results[0])
+}
+
+const displayUserDetails = user => {
+    const userInfoContainer = document.getElementById('user-info')
+    userInfoContainer.textContent = '';
+    const div = document.createElement('div')
+    div.innerHTML = `
+<div class="card bg-info" style="width: 100;">
+        <img src="${user.picture.thumbnail}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h4 class="card-title">Name:- ${user.name.title} ${user.name.first} ${user.name.last}</h4>
+        <h6 class="card-title">Email:- ${user.email}</h6>
+        
+        <p class="card-text">Gender:- ${user.gender}</p>
+        <p class="card-text">Age:- ${user.registered.age}</p>
+        <p class="card-text">Country:- ${user.location.country}</p>
+        <p class="card-text">City:- ${user.location.city}</p>
+        <p class="card-text">Street:- ${user.location.street.name}, ${user.location.street.number} </p>
+        <p class="card-text">Phone:- ${user.phone}</p>
+        <p class="card-text">Cell:- ${user.cell}</p>
+        <p class="card-text">Postcode:- ${user.location.postcode}</p>
+        <p class="card-text">Timezone:- ${user.location.timezone.offset}</p>
+    </div>
+</div>
+    `
+    userInfoContainer.appendChild(div);
+}
